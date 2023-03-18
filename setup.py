c2332334109d19466eba3ed4328df763c401327d
skoulouzis <koulouzis@gmail.com>
2023-03-18 20:23:17 +0100

    added default_variables_library.yaml

--- setup.py
+++ setup.py
@@ -20,19 +20,20 @@ with open('multiply_core/version.py') as f:
     exec(f.read())
 
 setup_args = dict(name='multiply-core',
-      packages=setuptools.find_packages(),
-      version=__version__,
-      description='MULTIPLY Core',
-      author='MULTIPLY Team',
-      entry_points={
-          'observations_creators': [
-              's2_observation_creator = multiply_core.observations:s2_observations.S2ObservationsCreator',
-          ],
-          'variables': ['core_variables = multiply_core.variables:variables.get_default_variables'],
-          'aux_data_provider_creators': ['default_provider_creator = '
-                                         'multiply_core.util:aux_data_provision.DefaultAuxDataProviderCreator']
-      },
-      install_requires=requirements
+                  packages=setuptools.find_packages(),
+                  include_package_data = True,
+                  version=__version__,
+                  description='MULTIPLY Core',
+                  author='MULTIPLY Team',
+                  entry_points={
+                      'observations_creators': [
+                          's2_observation_creator = multiply_core.observations:s2_observations.S2ObservationsCreator',
+                      ],
+                      'variables': ['core_variables = multiply_core.variables:variables.get_default_variables'],
+                      'aux_data_provider_creators': ['default_provider_creator = '
+                                                     'multiply_core.util:aux_data_provision.DefaultAuxDataProviderCreator']
+                  },
+                  install_requires=requirements
       )
 
 if __name__ == "__main__":
